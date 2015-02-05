e31ab55dbe63a1b06f555c17b2af7bb7c2d5baea
josephjbliss <josephjbliss@gmail.com>
2015-02-04 21:35:50 -0500

    Cookie to close, Color changes

--- daily-dish-pro/js/signup.js
+++ daily-dish-pro/js/signup.js
@@ -1,12 +1,23 @@
 jQuery(function( $ ){
 	$(".widget.widget_mc4wp_widget").hide();
 
-	$(window).scroll(function() {  
-		if ($(window).scrollTop() > 0 && $(".widget.widget_mc4wp_widget").css("display") == "none") {
+	$("#close").click(function(){
+		$.cookie("closed", "closed");
+		$(".widget.widget_mc4wp_widget").stop().fadeOut(500);
+		$(".site-footer").css({marginBottom: 0});
+	});
+
+	$(window).scroll(function() {
+		var myHeight = $(".widget.widget_mc4wp_widget").height();
+		// alert(myHeight);
+
+		if ($(window).scrollTop() > 0 && $(".widget.widget_mc4wp_widget").css("display") == "none" && $.cookie("closed") != "closed") {
+			$(".site-footer").css({marginBottom: myHeight});
 			$(".widget.widget_mc4wp_widget").stop().fadeIn(500);
 		}
 		else if ($(window).scrollTop() == 0) {
 			$(".widget.widget_mc4wp_widget").stop().fadeOut(500);
+			$(".site-footer").css({marginBottom: 0});
 		}
 	}); 
 });
