9b0fea6bc8fd0eb3076a4fbf5d42b6362c6edce3
josephjbliss <josephjbliss@gmail.com>
2015-04-24 01:12:33 -0400

    Getting subscribe Mail Chimp form sorted.

--- daily-dish-pro/js/signup.js
+++ daily-dish-pro/js/signup.js
@@ -1,22 +1,22 @@
 jQuery(function( $ ){
-	$(".widget.widget_mc4wp_widget").hide();
+	$("#mc4wp-form-1").hide();
 
-	$("#close").click(function(){
+	$(".close").click(function(){
 		$.cookie("closed", "closed", { expires: 7 }); // Cookie expires in 1 week
-		$(".widget.widget_mc4wp_widget").stop().fadeOut(500);
+		$("#mc4wp-form-1").stop().fadeOut(500);
 		$(".site-footer").css({marginBottom: 0});
 	});
 
 	$(window).scroll(function() {
-		var myHeight = $(".widget.widget_mc4wp_widget").height();
+		var myHeight = $("#mc4wp-form-1").height();
 		// alert(myHeight);
 
-		if ($(window).scrollTop() > 0 && $(".widget.widget_mc4wp_widget").css("display") == "none" && $.cookie("closed") != "closed") {
+		if ($(window).scrollTop() > 0 && $("#mc4wp-form-1").css("display") == "none" && $.cookie("closed") != "closed") {
 			$(".site-footer").css({marginBottom: myHeight});
-			$(".widget.widget_mc4wp_widget").stop().fadeIn(500);
+			$("#mc4wp-form-1").stop().fadeIn(500);
 		}
 		else if ($(window).scrollTop() == 0) {
-			$(".widget.widget_mc4wp_widget").stop().fadeOut(500);
+			$("#mc4wp-form-1").stop().fadeOut(500);
 			$(".site-footer").css({marginBottom: 0});
 		}
 	}); 
