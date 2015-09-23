9d694f904d356d9e81f0658c2b66bf98d8d4fcf2
josephjbliss <josephjbliss@gmail.com>
2015-09-23 13:44:40 -0400

    Updating

--- js/project.js
+++ js/project.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
 	$(".button.email-submit").click(function(){
 		$(".mc4wp-form form").submit();
 	});
+
+	// NOT WORKING
+	// $("li.categories").click(function(){
+	// 	$(this).find("a").trigger("click");
+	// });
 });
