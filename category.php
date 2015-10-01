7b046336dadddf0927484a1c70405546e56daba5
josephjbliss <josephjbliss@gmail.com>
2015-10-01 00:49:53 -0400

    Categories page changes

--- category.php
+++ category.php
@@ -1,13 +1,27 @@
 <?php get_header(); ?>
-<section id="content" role="main">
-<header class="header">
-<h1 class="entry-title"><?php _e( 'Category Archives: ', 'invibed' ); ?><?php single_cat_title(); ?></h1>
-<?php if ( '' != category_description() ) echo apply_filters( 'archive_meta', '<div class="archive-meta">' . category_description() . '</div>' ); ?>
-</header>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
-</section>
-<?php get_sidebar(); ?>
+        <h4 class="category-title"><span class="fa fa-newspaper-o"></span><?php single_cat_title(); ?></h4>
+
+        <section class="clearfix wrap" id="cards-main">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <?php 
+        $post_thumbnail_id = get_post_thumbnail_id();
+        $post_thumbnail_url = wp_get_attachment_url( $post_thumbnail_id );
+        ?>
+
+            <a href="<?php the_permalink(); ?>">
+                <article class="cards">
+                    <div class="cards-img" style="background-image: url(<?php echo $post_thumbnail_url;?>)"></div>
+                    <header class="cards-header">
+                        <h4><span class="fa fa-diamond"></span><?php single_cat_title(); ?></h4>
+                        <h3><?php if (strlen($post->post_title) > 50) {
+                        echo substr(the_title($before = '', $after = '', FALSE), 0, 50) . '...'; } else {
+                        the_title();
+                        } ?></h3>
+                    </header>
+                </article>
+            </a>
+        <?php endwhile; endif; ?>
+        </section>
+
 <?php get_footer(); ?>
--- page-home.php
+++ page-home.php
@@ -31,7 +31,10 @@ Template Name: Home Page
 						<a href="<?php the_permalink(); ?>">
 	                    <article class="featured-article clearfix">
 	                        <h4><span class="fa fa-usd"></span>Category Name</h4>
-	                        <h2 class="featured-title"><?php the_title(); ?></h2>
+	                        <h2 class="featured-title"><?php if (strlen($post->post_title) > 50) {
+                        echo substr(the_title($before = '', $after = '', FALSE), 0, 50) . '...'; } else {
+                        the_title();
+                        } ?></h2>
 	                    </article>
 	                    </a>
 					    <?php endforeach; ?>
@@ -42,7 +45,36 @@ Template Name: Home Page
             </div>
         </section>
 
-<section id="content">
+        <div class="tagline">Know More. Spend Less. Live Better.</div>
 
-</section>
+        <section class="clearfix wrap" id="cards-main">
+		<?php
+		$args = array( 'posts_per_page' => 12, 'category' => 'explore' );
+		$myposts = get_posts( $args );
+		foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+
+
+        <?php 
+        $post_thumbnail_id = get_post_thumbnail_id();
+        $post_thumbnail_url = wp_get_attachment_url( $post_thumbnail_id );
+        ?>
+
+
+		            <a href="<?php the_permalink(); ?>">
+		                <article class="cards">
+		                    <div class="cards-img" style="background-image: url(<?php echo $post_thumbnail_url;?>)"></div>
+		                    <header class="cards-header">
+		                        <h4><span class="fa fa-diamond"></span><?php ?></h4>
+		                        <h3><?php if (strlen($post->post_title) > 50) {
+		                        echo substr(the_title($before = '', $after = '', FALSE), 0, 50) . '...'; } else {
+		                        the_title();
+		                        } ?></h3>
+		                    </header>
+		                </article>
+		            </a>
+
+
+		<?php endforeach; 
+		wp_reset_postdata();?>
+        </section>
 <?php get_footer(); ?>
